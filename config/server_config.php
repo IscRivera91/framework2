8a73806d0012798b99bf65a6df62d2d0c4fb12c4
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2020-05-12 19:28:16 -0500

    server nuevo

--- config/server_config.php
+++ config/server_config.php
@@ -5,9 +5,9 @@
     // config BD
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');
+	define('DB_PASSWORD', '12345678');
 	define('DB_NAME', 'framework2');
 
     // usuario y contra para el inicio de session
     define('DB_USER_SESSION', 'root');
-    define('DB_PASSWORD_SESSION', '');
+    define('DB_PASSWORD_SESSION', '12354678');
